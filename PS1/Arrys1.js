b493ec055f404985362158806bb851f8765890db
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-07-18 02:38:57 +0530

    push arrray file

---
+++ PS1/Arrys1.js
@@ -0,0 +1,54 @@
+const a = [1,2,3,4,5,6]
+console.log(`starting ${a}` )
+//join 
+let result1 = a.join("-")
+console.log(`after join ${result1}`)
+// pop()
+let result2 = a.pop()
+console.log(`pop result ${result2}`)
+console.log(`after pop  remaining element ${a}`)
+
+// push()
+
+let result3 = a.push(9)
+console.log(`after push ${result3}`)
+console.log(`after push add ele ${a}`)
+
+
+//shift 
+let result4 = a.shift()
+console.log(`after shift ${result4}`)
+console.log(`after shift  remaining element ${a}`)
+
+//unshift 
+let result5 = a.unshift(7,5,6)
+console.log(`after unshift ${result4}`)
+console.log(`after unshift  remaining element ${a}`)
+
+// splice 
+
+let result6 = a.splice(0,3,1)
+console.log(`after splice ${result6}`)
+console.log(`after splice  remaining element ${a}`)
+
+//slice 
+const a1 = [1,2,3,4,5,6]
+let result7 = a1.slice(2)
+console.log(`after slice ${result7}`)
+console.log(`after slice  remaining element ${a1}`)                                                                          
+
+let b = new Array (1,4,5,6,7)
+console.log(b)
+
+b[1]=2
+console.log(b)
+
+// concat
+
+let result8 = a.concat(b)
+console.log(`after concat existing array ${result8}`)
+
+// reverse () 
+
+let result9=a.reverse()
+console.log(result9)
