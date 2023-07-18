d709040c5339a0831e71e96035ca05732f70851a
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-07-18 20:31:07 +0530

    add code

--- PS1/Arrys1.js
+++ PS1/Arrys1.js
@@ -1,54 +1,65 @@
-const a = [1,2,3,4,5,6]
-console.log(`starting ${a}` )
-//join 
-let result1 = a.join("-")
-console.log(`after join ${result1}`)
-// pop()
-let result2 = a.pop()
-console.log(`pop result ${result2}`)
-console.log(`after pop  remaining element ${a}`)
+// const a = [1,2,3,4,5,6]
+// console.log(`starting ${a}` )
+// //join 
+// let result1 = a.join("-")
+// console.log(`after join ${result1}`)
+// // pop()
+// let result2 = a.pop()
+// console.log(`pop result ${result2}`)
+// console.log(`after pop  remaining element ${a}`)
 
-// push()
+// // push()
 
-let result3 = a.push(9)
-console.log(`after push ${result3}`)
-console.log(`after push add ele ${a}`)
+// let result3 = a.push(9)
+// console.log(`after push ${result3}`)
+// console.log(`after push add ele ${a}`)
 
 
-//shift 
-let result4 = a.shift()
-console.log(`after shift ${result4}`)
-console.log(`after shift  remaining element ${a}`)
+// //shift 
+// let result4 = a.shift()
+// console.log(`after shift ${result4}`)
+// console.log(`after shift  remaining element ${a}`)
 
-//unshift 
-let result5 = a.unshift(7,5,6)
-console.log(`after unshift ${result4}`)
-console.log(`after unshift  remaining element ${a}`)
+// //unshift 
+// let result5 = a.unshift(7,5,6)
+// console.log(`after unshift ${result4}`)
+// console.log(`after unshift  remaining element ${a}`)
 
-// splice 
+// // splice 
 
-let result6 = a.splice(0,3,1)
-console.log(`after splice ${result6}`)
-console.log(`after splice  remaining element ${a}`)
+// let result6 = a.splice(0,3,1)
+// console.log(`after splice ${result6}`)
+// console.log(`after splice  remaining element ${a}`)
 
-//slice 
-const a1 = [1,2,3,4,5,6]
-let result7 = a1.slice(2)
-console.log(`after slice ${result7}`)
-console.log(`after slice  remaining element ${a1}`)                                                                          
+// //slice 
+// const a1 = [1,2,3,4,5,6]
+// let result7 = a1.slice(2)
+// console.log(`after slice ${result7}`)
+// console.log(`after slice  remaining element ${a1}`)                                                                          
 
-let b = new Array (1,4,5,6,7)
-console.log(b)
+// let b = new Array (1,4,5,6,7)
+// console.log(b)
 
-b[1]=2
-console.log(b)
+// b[1]=2
+// console.log(b)
 
-// concat
+// // concat
 
-let result8 = a.concat(b)
-console.log(`after concat existing array ${result8}`)
+// let result8 = a.concat(b)
+// console.log(`after concat existing array ${result8}`)
 
-// reverse () 
+// // reverse () 
 
-let result9=a.reverse()
-console.log(result9)
+// let result9=a.reverse()
+// console.log(result9)
+
+
+let obj1 = {
+    Name: "vaibhav",
+    Pass: "1234"
+  }
+  let obj2 = obj1
+  obj2.Pass="321"
+  console.log(obj1)
+  console.log(obj2)
+  //njkjk
